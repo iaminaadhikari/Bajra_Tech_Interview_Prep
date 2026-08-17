7e8e3ba1d88fca34775586ed585c41681e215f01
Ina Adhikari <inaadhikari78@gmail.com>
2026-08-17 15:02:20 +0545

    Add/Update components/Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { BookOpen, PenTool, Users, Timer } from 'lucide-react';
+import { BookOpen, PenTool, Users, Timer, LayoutTemplate } from 'lucide-react';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
@@ -14,6 +14,9 @@ export default function Navbar() {
         </Link>
         <Link href="/aptitude" className={styles.link}>
           <PenTool size={18} /> Aptitude
+        </Link>
+        <Link href="/uiux-bank" className={styles.link}>
+          <LayoutTemplate size={18} /> UI/UX Bank
         </Link>
         <Link href="/on-site" className={styles.link}>
           <BookOpen size={18} /> On-Site
